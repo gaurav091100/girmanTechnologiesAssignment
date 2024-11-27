2d30cb9d12b478f9bc87c9a3b6f535d930a9260e
gaurav091100 <gaurav091100@gmail.com>
2024-11-28 00:18:58 +0530

    initialized proejct

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import logo from "../../assets/logo.svg";
+
+const Header = () => {
+  return (
+    <header>
+        <div>
+            <img src={logo} alt="logo" />
+        </div>
+        <div></div>
+    </header>
+  )
+}
+
+export default Header
